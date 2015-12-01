8da57b7d58ed5e9501ce11aec3d7f810452488bd
AlexPrya <pruashnikov@gmail.com>
2015-12-02 01:11:52 +0300

    bugfix

--- js/script.js
+++ js/script.js
@@ -4,12 +4,16 @@
 
 $('.left-slider-body').owlCarousel({
     items:1,
-    nav:true
+    dots: false,
+    nav:false
 });
 
 $('.main-slider').owlCarousel({
     nav:false,
     dots: true,
+    autoPlay: true,
+    autoplayHoverPause: true,
+    dotsSpeed: 500,
     items:1
 });
 
